f9b4576c9d5ceb9916089e9f142d32d636785315
brindhavanan <brindhavanan@gmail.com>
2023-09-07 19:47:22 +0530

    newly added

---
+++ src/org/test/GitProgram.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class GitProgram {
+	
+	private void name() {
+		System.out.println("Name : Brindhavanan");
+	}
+	private void course() {
+		System.out.println("Course : Java");
+
+	}
+	public static void main(String[] args) {
+		
+		GitProgram g = new GitProgram();
+		g.name();
+		g.course();
+		
+	}
+
+}
